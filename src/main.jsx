68284d55e11dde1c74c38baf36c20468dfa031c8
Aashray Katiyar <aashray446@gmail.com>
2024-04-20 20:13:11 +0530

    FIX : basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import LandingPage from "./pages/landingPage";
+import './index.css';
+import Results from "./pages/results";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage></LandingPage>
+  },
+  {
+    path: "/result",
+    element: <Results></Results>
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
