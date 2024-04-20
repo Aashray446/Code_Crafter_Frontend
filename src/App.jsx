3f7d2df84ebce5c1b7b7bccddddbfd0431fc00ca
Aashray Katiyar <aashray446@gmail.com>
2024-04-20 18:42:42 +0530

    INIT : Frontend repo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>TestTame</h1>
+      <div className="flex justify-center items-center h-full p-4">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="py-2 px-4 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+    </>
+  )
+}
+
+export default App
